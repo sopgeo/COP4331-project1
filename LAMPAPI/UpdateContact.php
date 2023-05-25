f497aa7e5dab5f65d61837ee4a0d85871bec335b
Joe Nguyen <80282367+JoeNguyen1@users.noreply.github.com>
2023-05-24 21:44:12 -0400

    Add files via upload

---
+++ LAMPAPI/UpdateContact.php
@@ -0,0 +1,42 @@
+<?php
+	$inData = getRequestInfo();
+	
+
+	$name = $inData["name"];
+	//$userId = $inData["userId"];
+	$phone = $inData["phone"];
+	$email = $inData["email"];
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("UPDATE Contacts SET Name=?, Phone=?, WHERE Email=?");
+		$stmt->bind_param("sss", $name, $phone, $email);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
